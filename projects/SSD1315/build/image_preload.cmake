22da4d4c764fd7affe4b6933d3e495f519fd41c1
Dominik Puck <93704949+123Dome@users.noreply.github.com>
2024-11-26 01:56:28 +0100

    Delete projects/SSD1315/build directory

--- projects/SSD1315/build/image_preload.cmake
+++
@@ -1,5 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf9160dk/nrf9160/ns" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/ncs/IoT-Security/projects/SSD1315/boards/nrf9160dk_nrf9160_ns.overlay" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
