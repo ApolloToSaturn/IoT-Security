629fade520e7d06f38bfcb89a92bef74fb4c278e
Dome <dominik.puck@web.de>
2024-11-28 18:47:32 +0100

    Funktionen Ausgelagert

---
+++ projects/SSD1315/include/draw.h
@@ -0,0 +1,11 @@
+// draw.h
+#ifndef DRAW_H
+#define DRAW_H
+
+#include "ssd1315.h"
+
+void draw_chess_pattern(SSD1315_Object_t *obj);
+void draw_random_pattern(SSD1315_Object_t *obj);
+void draw_hbrs_logo(SSD1315_Object_t *obj);
+
+#endif // DRAW_H
--- projects/SSD1315/src/main.c
+++ projects/SSD1315/src/main.c
@@ -9,7 +9,7 @@
 #include "ssd1315.h"
 #include "i2c_helper.h"
 
-#include "hbrslogo.h"
+#include "draw.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -25,17 +25,18 @@ SSD1315_IO_t ssd1315_io = {
     .GetTick = (int32_t (*)(void))k_uptime_get_32 // Zephyr-Zeitfunktion
 };
 
-void init_display(void) {
+int init_display(void) {
     LOG_INF("Initializing SSD1315");
     if (SSD1315_RegisterBusIO(&ssd1315_obj, &ssd1315_io) != SSD1315_OK) {
         LOG_ERR("Failed to register SSD1315 IO");
-        return;
+        return -1;
     }
     if (SSD1315_Init(&ssd1315_obj, SSD1315_FORMAT_DEFAULT, SSD1315_ORIENTATION_LANDSCAPE) != SSD1315_OK) {
         LOG_ERR("Failed to initialize SSD1315");
-        return;
+        return -1;
     }
     LOG_INF("SSD1315 initialized successfully");
+    return 0;
 }
 
 
@@ -47,30 +48,13 @@ int main(void) {
 
     SSD1315_DisplayOn(&ssd1315_obj);
 
-    LOG_INF("Chess Pattern");
-    SSD1315_Clear(&ssd1315_obj, SSD1315_COLOR_BLACK);
-    for (uint32_t y = 0; y < 64; y += 8) {
-        for (uint32_t x = 0; x < 128; x += 8) {
-            SSD1315_FillRect(&ssd1315_obj, x, y, 8, 8, (x + y) % 16 == 0 ? SSD1315_COLOR_WHITE : SSD1315_COLOR_BLACK);
-        }
-    }
-    SSD1315_Refresh(&ssd1315_obj);
+    draw_chess_pattern(&ssd1315_obj);
     k_sleep(K_SECONDS(2));
 
-    LOG_INF("Random Pattern");
-    SSD1315_Clear(&ssd1315_obj, SSD1315_COLOR_BLACK);
-    for (uint32_t y = 0; y < SSD1315_LCD_PIXEL_HEIGHT; y++) {
-        for (uint32_t x = 0; x < SSD1315_LCD_PIXEL_WIDTH; x++) {
-            SSD1315_SetPixel(&ssd1315_obj, x, y, (x + y) % 4 ? SSD1315_COLOR_WHITE : SSD1315_COLOR_BLACK);
-        }
-    }
-    SSD1315_Refresh(&ssd1315_obj);
+    draw_random_pattern(&ssd1315_obj);
     k_sleep(K_SECONDS(3));
 
-    LOG_INF("HBRS Logo");
-    SSD1315_Clear(&ssd1315_obj, SSD1315_COLOR_BLACK);
-    SSD1315_DrawBitmap(&ssd1315_obj, 0, 0, hbrslogo_bits);
-    SSD1315_Refresh(&ssd1315_obj);
+    draw_hbrs_logo(&ssd1315_obj);
 
     while (1) {
         k_sleep(K_SECONDS(1));
